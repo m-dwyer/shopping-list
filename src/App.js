3fdbda3424574cdf73a7dfebe233e4eb8f27a368
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2020-06-10 21:52:09 +1000

    Basic delete button styling

--- src/App.js
+++ src/App.js
@@ -89,8 +89,8 @@ class ItemsList extends React.Component {
             Object.entries(this.props.items).map((item, idx) => {
               return (
                 <li className="item" key={item[0]}>
-                  {item[1]}
-                  <button onClick={() => this.props.handleDelete(item[0])}>X</button>
+                  <span>{item[1]}</span>
+                  <button className="item-delete" onClick={() => this.props.handleDelete(item[0])}>X</button>
                 </li>
               );
             })
